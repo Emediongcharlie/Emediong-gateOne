77aa45bab5e5c7a9328b486a992aaa05eea039e8
Emediong <emediongcharliegmail.com>
2024-05-08 10:11:34 +0100

    java

---
+++ Day 2/Java/TaskFour.java
@@ -0,0 +1,13 @@
+public class TaskFour{
+public static int [] arrays = int array[]  {
+	
+	int [] array = new int[array.length / 2];
+	int[] even = 0;
+	for(int index = 0; index <= array.length; index++){
+		if(index % 2 == 0){
+		even = array[index];
+		}
+		}
+	return even;
+}
+}
