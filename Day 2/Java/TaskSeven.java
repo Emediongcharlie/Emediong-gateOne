38fcfa386f310a50e1e96592410ba6b47b2354f2
Emediong <emediongcharliegmail.com>
2024-05-08 10:12:39 +0100

    java

---
+++ Day 2/Java/TaskSeven.java
@@ -0,0 +1,14 @@
+public class TaskSeven{
+public static int [] arrays = int [] array{
+	
+	int [] array = new int[array.length / 2]
+	int[] odd = 0;
+	int sum = 0;
+	for(int index = 0; index <= array.length; index++){
+		if(index % 2 == 1){
+		sum += array[index];
+		}
+		}
+	return sum
+	}
+}
