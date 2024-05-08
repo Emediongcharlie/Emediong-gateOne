5c3c26692aa0830eedf40fae0c7728da4b74e990
Emediong <emediongcharliegmail.com>
2024-05-08 10:30:53 +0100

    java

---
+++ Day 1/Java/TaskThirteen.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TaskThirteen{
+
+public static void main(String[]args){
+
+	Scanner input = new Scanner(System.in);
+	int sum = 0;
+	int even = 0;	
+
+	for(int i = 1; i < 10; i++){
+	if(i % 2 == 1){	
+	System.out.print(i + " ");
+
+	}
+	}
+	
+
+
+}
+}
