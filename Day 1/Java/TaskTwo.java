1ef9b43522ea2c7a08768c067af306ac9a8bbcea
Emediong <emediongcharliegmail.com>
2024-05-08 10:22:47 +0100

    java

---
+++ Day 1/Java/TaskTwo.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class TaskTwo{
+
+public static void main(String[]args){
+
+	Scanner input = new Scanner(System.in);
+	int sum = 0;
+	double average = 0;	
+
+	for(int i = 0; i < 10; i++){
+	System.out.print("Enter Scores:");
+	int scores = input.nextInt();
+	sum = sum + scores;
+	}
+	
+	average = (double)sum / 10;
+
+	System.out.print("Average is = " + average);
+
+}
+}
