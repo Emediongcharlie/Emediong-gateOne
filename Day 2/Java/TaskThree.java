7c5ba9785e43181ce7ca4b1d85ce2675633b9947
Emediong <emediongcharliegmail.com>
2024-05-08 10:14:09 +0100

    java

---
+++ Day 2/Java/TaskThree.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class TaskThree{
+public static void main(String[] args){
+	
+	 
+	int [] arrays = {1,2,3,4,5,6,7,8,9,2};
+
+	for(int index = 0; index < arrays.length; index++){ 
+	System.out.println(arrays[index] + " ");
+	}
+	}
+}
