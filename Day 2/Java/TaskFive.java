6ca55d4ca07c98ea0cd500420e6542fd3d69058b
Emediong <emediongcharliegmail.com>
2024-05-08 10:09:34 +0100

    java

---
+++ Day 2/Java/TaskFive.java
@@ -0,0 +1,13 @@
+public class TaskFive{
+public static int [] arrays = int [] array{
+	
+	int [] array = new int[array.length / 2]
+	int[] even = 0;
+	for(int index = 0; index <= array.length; index++){
+		if(index % 2 == 1){
+		even = array[index];
+		}
+		}
+	return even
+	}
+}
