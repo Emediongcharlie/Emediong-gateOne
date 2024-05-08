0529895a66d9921ce9eac241c711d8b61801b714
Emediong <emediongcharliegmail.com>
2024-05-08 10:33:18 +0100

    java

---
+++ Day 1/Java/TaskFourteen.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TaskFourteen{
+
+public static void main(String[]args){
+
+	Scanner input = new Scanner(System.in);
+	int sum = 0;
+	int even = 0;	
+
+	for(int i = 1; i < 10; i++){
+	if(i % 4 == 0){	
+	System.out.print(i + " ");
+
+	}
+	}
+	
+
+
+}
+}
