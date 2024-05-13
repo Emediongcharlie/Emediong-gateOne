41a17cb3343bdbdbc5574fe47cc6be6a36030a40
Emediong <emediongcharliegmail.com>
2024-05-13 18:57:55 +0100

    second update

--- Day7/Java/MenstrualCycle.java
+++ Day7/Java/MenstrualCycle.java
@@ -11,30 +11,36 @@ public static void main(String[] args){
 	System.out.print("What is your name:");
 	String name = input.next();
 
-	System.out.print("What was the first day of last two month's cycle(2024 - 12 - 1):");
+	System.out.print("What was the first day of last two month's cycle(1-1-1991):");
 	String lastTwoMonthDate = input.next();
+	
+	//char charZero= lastTwoMonthDate.charAt(0);
+	//int firstChar = Integer.parseInt(charZero);
 
-	System.out.print("What was the first day of last month's cycle(2024 - 12 - 1):");
+	System.out.print("What was the first day of last month's cycle(1-1-1991):");
 	String lastMonthDate = input.next();
 
 	System.out.print("How many days did the last period last:");
 	int days = input.nextInt();
 	
-	LocalDate today = LocalDate.now();
-	System.out.print("Todays's date " + today);
-	
-	//int date = Integer.parseInt(lastMonthDate);
 
-	//LocalDate dates = LocalDate.of(date);
+	LocalDate today = LocalDate.now();
+	System.out.println("Todays's date " + today);
 	
 	LocalDate oneMonth = today.plusMonths(1);
-	System.out.print("One Month from now is: " + oneMonth);
+	System.out.println("One Month from now is: " + oneMonth);
 
-	LocalDate date = LocalDate.parse(lastTwoMonthDate);
-	LocalDate secondDate = LocalDate.parse(lastMonthDate);
+	//LocalDate ovulationPeriod = secondDate.minusDays(14);
+	//System.out.println("Ovulation date is :" + ovulationPeriod);
 
-	LocalDate ovulationPeriod = secondDate.minusDays(14);
-	System.out.print("Ovulation date is :" + ovulationPeriod);
+	
+	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	LocalDate localLastTwoMonthDate = LocalDate.parse(lastMonthDate, formatter);
+	LocalDate localLastMonthDate = LocalDate.parse(lastMonthDate, formatter);
+
+	Period monthsPeriod = Period.between(localLastTwoMonthDate, localLastMonthDate);
+	System.out.println("Difference beween two previous period  " + monthsPeriod.getDays());
+	
 	
 }
 }
