a2485537b490388db003ed5a8985a60bdab43daf
Emediong <emediongcharliegmail.com>
2024-05-14 03:01:59 +0100

    new update

--- Day7/Java/MenstrualCycle.java
+++ Day7/Java/MenstrualCycle.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class MenstrualCycle{
 public static void main(String[] args){
@@ -11,13 +12,13 @@ public static void main(String[] args){
 	System.out.print("What is your name:");
 	String name = input.next();
 
-	System.out.print("What was the first day of last two month's cycle(1-1-1991):");
+	System.out.print("What was the first day of last two month's cycle(01-01-1991):");
 	String lastTwoMonthDate = input.next();
 	
 	//char charZero= lastTwoMonthDate.charAt(0);
 	//int firstChar = Integer.parseInt(charZero);
 
-	System.out.print("What was the first day of last month's cycle(1-1-1991):");
+	System.out.print("What was the first day of last month's cycle(01-01-1991):");
 	String lastMonthDate = input.next();
 
 	System.out.print("How many days did the last period last:");
@@ -32,19 +33,25 @@ public static void main(String[] args){
 
 	
 	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-	LocalDate localLastTwoMonthDate = LocalDate.parse(lastMonthDate, formatter);
+	LocalDate localLastTwoMonthDate = LocalDate.parse(lastTwoMonthDate, formatter);
 	LocalDate localLastMonthDate = LocalDate.parse(lastMonthDate, formatter);
 
 	LocalDate ovulationPeriod = localLastMonthDate.minusDays(14);
 	System.out.println("Ovulation date is :" + ovulationPeriod);
 
 	Period monthsPeriod = Period.between(localLastTwoMonthDate, localLastMonthDate);
-	System.out.println(monthsPeriod.getYears());
+	//long result = ChronoUnit.DAYS.between(localLastTwoMonthDate, localLastMonthDate);
+	System.out.println(monthsPeriod.getDays());
+	
 	
 	LocalDate fertilePeriod = ovulationPeriod.minusDays(5);
 	LocalDate fertilePeriodEnd = ovulationPeriod.plusDays(1);
-	System.out.print("Fertile period is between " + fertilePeriod + " and " + fertilePeriodEnd);
+	System.out.println("Fertile period is between " + fertilePeriod + " and " + fertilePeriodEnd);
 	
+	
+	LocalDate safePeriod = ovulationPeriod.minusDays(6);
+	LocalDate safePeriodEnd = ovulationPeriod.plusDays(2);
+	System.out.println("Safe period is between " + today + "and" + safePeriod + " and between " + fertilePeriodEnd);
 	
 }
 }
