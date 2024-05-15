9b8b6c6197fd7185d7d53565ce4e12df1c870e9c
Emediong <emediongcharliegmail.com>
2024-05-15 05:16:23 +0100

    menstrual cycle

---
+++ Day7/Python/menstrual_app.py
@@ -0,0 +1,7 @@
+from datetime import date, datetime, timedelta
+
+last_two_month_start_date = input("Enter the first day of your cycle two months ago:")
+last_month_start_date = input("Enter the first day of your last months cycle:")
+
+today = date.today()
+print(today)
