450ac6bc72d7070f3d2f9d32944cc900a2c67ab2
Emediong <emediongcharliegmail.com>
2024-05-16 08:12:32 +0100

    updated

--- Day7/Java/MenstrualCycle.java
+++ Day7/Java/MenstrualCycle.java
@@ -8,6 +8,11 @@ public class MenstrualCycle{
 public static void main(String[] args){
 
 	Scanner input = new Scanner(System.in);
+	
+	System.out.println("============================================================");
+	System.out.println("<<<<<<WELCOME TO ACCURATE MENTRUAL APP>>>>>>");
+	System.out.println("============================================================");
+
 
 	System.out.print("What is your name:");
 	String name = input.next();
@@ -15,23 +20,21 @@ public static void main(String[] args){
 	System.out.print("What was the first day of last two month's cycle(01-01-1991):");
 	String lastTwoMonthDate = input.next();
 	
-	//char charZero= lastTwoMonthDate.charAt(0);
-	//int firstChar = Integer.parseInt(charZero);
-
 	System.out.print("What was the first day of last month's cycle(01-01-1991):");
 	String lastMonthDate = input.next();
 
-	System.out.print("How many days did the last period last:");
+	System.out.print("How many days did the last period flow last:");
 	int days = input.nextInt();
-	
+		System.out.println("============================================================");
+	System.out.println("           *RESULT*   ");
 
-	LocalDate today = LocalDate.now();
-	System.out.println("Todays's date is " + today);
-	
-	LocalDate oneMonth = today.plusMonths(1);
-	System.out.println("One Month from now is: " + oneMonth);
 
+	//LocalDate today = LocalDate.now();
+	//System.out.println("Todays's date is " + today);
 	
+	//LocalDate oneMonth = today.plusMonths(1);
+	//System.out.println("One Month from now is: " + oneMonth);
+
 	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 	LocalDate localLastTwoMonthDate = LocalDate.parse(lastTwoMonthDate, formatter);
 	LocalDate localLastMonthDate = LocalDate.parse(lastMonthDate, formatter);
@@ -43,6 +46,10 @@ public static void main(String[] args){
 
 	LocalDate nextCycleBegins = localLastMonthDate.plusDays(result);
 	System.out.println("Date of next cycle is " + nextCycleBegins); 
+	
+	LocalDate flowEnds = nextCycleBegins.plusDays(days);
+
+	System.out.println("Flow date is from " + nextCycleBegins + " to " + flowEnds);
 
 	LocalDate ovulationPeriod = nextCycleBegins.minusDays(14);
 	System.out.println("Ovulation date is :" + ovulationPeriod);
@@ -54,7 +61,13 @@ public static void main(String[] args){
 	LocalDate safePeriod = ovulationPeriod.minusDays(6);
 	LocalDate safePeriodEnd = ovulationPeriod.plusDays(2);
 	//System.out.println("Safe period is between " + today + " and " + safePeriod + " and between " + fertilePeriodEnd);
-	System.out.println("Every other day outside the fertile period is safe");
+	System.out.println("Every other day outside the fertile period is your safe period");
+
+		System.out.println("============================================================");
+	System.out.println("    THANK YOU FOR TRUSTING US   ");
+	System.out.println("============================================================");
+
+
 }
 }
 	
